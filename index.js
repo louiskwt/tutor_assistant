dbdf4b2e1f9d1ffc44d476657b54893a0ff8ae52
Louis Tsang <kawingt2@gmail.com>
2023-09-28 23:18:11 +0800

    set up ejs and tailwind

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.static("node_modules"));
 
 app.get("/", (req, res) => {
-  res.send("Hello Yo-Tutor!");
+  res.render(path.join(__dirname, "views", "index"));
 });
 
 app.listen(port, () => {
