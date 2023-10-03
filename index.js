a2db41d8e3a476fb915df414e69707dad50ca8c4
Louis Tsang <kawingt2@gmail.com>
2023-10-03 22:04:38 +0800

    added nodemon

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 app.get("/", (req, res) => {
   res.render("landingpage");
 });
