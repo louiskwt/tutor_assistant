409731cd1b196ef8a06ac60dc205f8fb54544973
Louis Tsang <kawingt2@gmail.com>
2023-10-03 23:04:57 +0800

    swaped out express

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./public/**/*.{html,js,css}", "./src/**/*.{html,js,css}", "./node_modules/tw-elements/dist/js/**/*.js"],
-  darkMode: "class",
-  plugins: [require("")],
-};
