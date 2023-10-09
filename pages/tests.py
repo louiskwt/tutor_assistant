7c856802931a9fb6bd5c55c5e6f386d29b9947b0
Louis Tsang <kawingt2@gmail.com>
2023-10-09 23:53:42 +0800

    wrote test

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class PageTests(TestCase):
+    def test_url_exists_at_correct_location(self):
+        res = self.client.get("/")
+        self.assertEqual(res.status_code, 200)
+    
+    def test_homepage_view(self):
+        res = self.client.get(reverse("home"))
+        self.assertEqual(res.status_code, 200)
+        self.assertTemplateUsed(res, "home.html")
