0154bb62ebc3283c657cb56248ddf9a4a34397a2
Louis Tsang <kawingt2@gmail.com>
2023-10-11 23:40:43 +0800

    set up navbar for dashboard

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import DashboardView, HomePageView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
+    path("test-dashboard", DashboardView.as_view(), name="dashboard")
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+# Testing only
+class DashboardView(TemplateView):
+    template_name = 'dashboard.html'
